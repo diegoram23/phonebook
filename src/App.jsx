5ad96a46311f1225fbbb765ba576c157e3e9913d
diegoram23 <diego.pta.oc@gmail.com>
2023-03-03 09:17:37 -0800

    initial layout and adding name to phonebook working

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [persons, setPersons] = useState([
+    {
+      "name": "Arto Hellas",
+      "number": "040-3424324",
+      "id": 1
+    }
+  ])
+
+  const [newName, setNewName] = useState('')
+
+  const handleNewName = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    const personObj = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(personObj))
+    setNewName('')
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          Name: <input
+            value={newName}
+            onChange={handleNewName}
+          />
+        </div>
+        <div>
+          Number: <input
+            value={newName}
+            onChange={handleNewName}
+          />
+        </div>
+        <div>
+          <button type='submit'>Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>
+        <p key={person.id}>{person.name} {person.number}</p>
+      )}
     </div>
   )
 }
