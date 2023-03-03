a16809706f7bb0969b45e9596f7786305f0fc809
diegoram23 <diego.pta.oc@gmail.com>
2023-03-03 09:34:56 -0800

    Add phone number function working

--- src/App.jsx
+++ src/App.jsx
@@ -11,37 +11,54 @@ function App() {
   ])
 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleNewName = (e) => {
+  const handleSubmitPerson = (e) => {
+    e.preventDefault()
+    const match = persons.find(person => person.name === newName)
+    if (match) {
+      alert(`${newName} is already in your contacts`);
+    } else {
+      addPerson()
+    }
+  }
+
+  const handleNameChange = (e) => {
     setNewName(e.target.value)
   }
 
-  const addPerson = (e) => {
-    e.preventDefault()
+  const handleNewNumber = (e) => {
+    setNewNumber(e.target.value)
+  }
+
+  const addPerson = () => {
     const personObj = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
     setPersons(persons.concat(personObj))
     setNewName('')
+    setNewNumber('')
   }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
+      <form onSubmit={handleSubmitPerson}>
         <div>
           Name: <input
             value={newName}
-            onChange={handleNewName}
+            onChange={handleNameChange}
           />
         </div>
-        <div>
+         <div>
           Number: <input
-            value={newName}
-            onChange={handleNewName}
+            value={newNumber}
+            onChange={handleNewNumber}
           />
-        </div>
+        </div> 
         <div>
           <button type='submit'>Add</button>
         </div>
