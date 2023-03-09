21ea53a0602e9df7f1a99acd4d29165078cc5c57
diegoram23 <diego.pta.oc@gmail.com>
2023-03-09 10:54:39 -0800

    now connecting to server with cors

--- src/contacts.js
+++ src/contacts.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3005/persons'
+const baseUrl = 'http://localhost:3002/api/persons'
 
 const getAll = () => {
     return axios.get(baseUrl)
